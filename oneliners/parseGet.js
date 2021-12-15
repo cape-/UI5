4ba9fd61ebff4e95ec3f1f81a1f7e165129e0e30
Lautaro Capella <laucape@gmail.com>
2021-12-15 00:24:22 -0300

    updated jsdoc

--- oneliners/parseGet.js
+++ oneliners/parseGet.js
@@ -1,6 +1,7 @@
 /**
-* Parses a GET request inside a $batch POST request
+* SAP UI5 Tool: Parses a GET request inside a $batch POST request
 * @param  {string} getString An string starting with "GET" and ending with "HTTP/X.X"
 * @returns {Object} An object containing `entity` and `[parameters]` properties
+* @author Lautaro Capella <laucape@gmail.com>
 */
 const parseGet = url => url.split(' ').find(s => s.includes("?")).split("?").reduce((a,c,i,arr) => a ? a : { entity: '/' + arr.shift(), parameters: arr.join(' ').split("&").map(s => s.split("=").reduce((a,c,i,arr) => a ? a : {key: arr[0], value: unescape(arr[1])}, null)) },null);
