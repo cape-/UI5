0927b7a2b95747c2e6aed49cdee4c90b7b0efb8c
Lautaro Capella <laucape@gmail.com>
2022-02-08 10:49:33 -0300

    Update formatDate.js

--- formatters/formatDate.js
+++ formatters/formatDate.js
@@ -1,3 +1,4 @@
-        formatDate: function(oDate, sLocale = 'es-cl') {
+        formatDate: function(sDate, sLocale = 'es-cl') {
+        	var oDate = new Date(sDate);
         	return oDate.toLocaleDateString(sLocale);
         },
