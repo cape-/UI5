3a7d21507261151b84868a1f828c90093bc4f8b1
Lautaro Capella <laucape@gmail.com>
2022-02-08 11:24:40 -0300

    add handle invalid date

--- formatters/formatDate.js
+++ formatters/formatDate.js
@@ -1,4 +1,4 @@
         formatDate: function(sDate, sLocale = 'es-cl') {
         	var oDate = new Date(sDate);
-        	return oDate.toLocaleDateString(sLocale);
+        	return !isNaN(oDate.getTime()) ? oDate.toLocaleDateString(sLocale) : '';
         },
