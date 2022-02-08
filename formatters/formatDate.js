adbb4bf03565b5b8184c3de5f3c28f8e5678843e
Lautaro Capella <laucape@gmail.com>
2022-02-08 12:18:40 -0300

    add sDefault parameter

--- formatters/formatDate.js
+++ formatters/formatDate.js
@@ -1,4 +1,4 @@
-        formatDate: function(sDate, sLocale = 'es-cl') {
+        formatDate: function(sDate, sLocale = 'es-cl', sDefault) {
         	var oDate = new Date(sDate);
-        	return !isNaN(oDate.getTime()) ? oDate.toLocaleDateString(sLocale) : '';
+        	return !isNaN(oDate.getTime()) ? oDate.toLocaleDateString(sLocale) : sDefault || '';
         },
