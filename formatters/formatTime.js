6de9f08f6644037bb65f4ff873b58394b488dbab
Lautaro Capella <laucape@gmail.com>
2022-02-08 10:38:06 -0300

    Update formatTime.js

--- formatters/formatTime.js
+++ formatters/formatTime.js
@@ -1,11 +1,11 @@
         formatTime: function(oTime) {
         	var h = Math.floor(oTime.ms/1000/60/60),
-            m = Math.floor((oTime.ms/1000/60/60 - h)*60),
-            s = Math.floor(((oTime.ms/1000/60/60 - h)*60 - m)*60);
+				m = Math.floor((oTime.ms/1000/60/60 - h)*60),
+				s = Math.floor(((oTime.ms/1000/60/60 - h)*60 - m)*60);
+			
+			h = h < 10 ? `0${h}`: h;
+			m = m < 10 ? `0${m}`: m;
+			s = s < 10 ? `0${s}`: s;
 
-          s = s < 10 ? `0${s}`: s;
-          m = m < 10 ? `0${m}`: m;
-          h = h < 10 ? `0${h}`: h;
-
-        	return `${s}:${m}:${h}`;
+        	return `${h}:${m}:${s}`;
         },
