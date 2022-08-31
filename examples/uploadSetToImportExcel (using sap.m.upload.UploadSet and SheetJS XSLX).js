10fae95af259dc208b11b8b2afc1cd10701fc6ac
Lautaro Capella <laucape@gmail.com>
2022-08-31 09:26:32 -0300

    fix correct sheet_to_json fn options argument

--- examples/uploadSetToImportExcel (using sap.m.upload.UploadSet and SheetJS XSLX).js
+++ examples/uploadSetToImportExcel (using sap.m.upload.UploadSet and SheetJS XSLX).js
@@ -1,24 +1,27 @@
     // in view declare an sap.m.upload.UploadSet with id="DocumentUploadSet" and uploadUrl=""
-    onInit() {
-			const oDocUploadSet = this.byId("DocumentUploadSet");
-			const oFileUploader = oDocUploadSet.getDefaultFileUploader();
-			this.configureFileUploader(oFileUploader);
-		},
-		configureFileUploader(oFileUploader) {
-			oFileUploader.attachChange(function (oEvent) {
-				const oFile = oEvent.getParameter("files") && oEvent.getParameter("files")[0];
-				if (oFile && window.FileReader) {
-					const oFileReader = new FileReader();
-					oFileReader.onload = this.onFileLoaded.bind(this);
-					oFileReader.readAsArrayBuffer(oFile);
-				}
-			}.bind(this));
-		},
-		onFileLoaded(oEvent) {
-			const aColNames =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); // ["A","B","C","D"...]
-			const aFileContent = oEvent.target.result; // file content as ArrayBuffer
-			const oWorkbook = XLSX.read(aFileContent);
-			 // aData will be an array of sheets, each one as an array of rows: [[{A:"", B:"", ...}]]
-			const aData = oWorkbook.SheetNames.map(sSheetName => XLSX.utils.sheet_to_json(oWorkbook.Sheets[sSheetName], aColNames));
-			debugger;
-		}
+    	onInit() {
+		const oDocUploadSet = this.byId("DocumentUploadSet");
+		const oFileUploader = oDocUploadSet.getDefaultFileUploader();
+		this.configureFileUploader(oFileUploader);
+	},
+	configureFileUploader(oFileUploader) {
+		oFileUploader.attachChange(function (oEvent) {
+			const oFile = oEvent.getParameter("files") && oEvent.getParameter("files")[0];
+			if (oFile && window.FileReader) {
+				const oFileReader = new FileReader();
+				oFileReader.onload = this.onFileLoaded.bind(this);
+				oFileReader.readAsArrayBuffer(oFile);
+			}
+		}.bind(this));
+	},
+	onFileLoaded(oEvent) {
+		const aColNames =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); // ["A","B","C","D"...]
+		const oSheetToJsonOptions = {
+			raw: false, 		// To get dates as date string and not int value
+			header: aColNames
+		};
+		const aFileContent = oEvent.target.result; // file content as ArrayBuffer
+		const oWorkbook = XLSX.read(aFileContent);
+		 // aData will be an array of sheets, each one as an array of rows: [[{A:"", B:"", ...}]]
+		const aData = oWorkbook.SheetNames.map(sSheetName => XLSX.utils.sheet_to_json(oWorkbook.Sheets[sSheetName], oSheetToJsonOptions));
+	}
